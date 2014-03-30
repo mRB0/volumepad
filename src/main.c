e06193fcd99bc3dcd892aae812746a59fc579789
Mike Burke <mrburke@gmail.com>
2014-03-30 13:21:59 -0300

    Add support for rotary dial volume control.  Volume control works on mac but not windows.

--- src/main.c
+++ src/main.c
@@ -7,6 +7,39 @@
 
 #include "usb_keyboard.h"
 
+// Multimedia keys aren't listed in usb_keyboard.h.
+#define KEY_VOLUME_UP 0x80
+#define KEY_VOLUME_DOWN 0x81
+
+struct debounce_state {
+    uint8_t state; // pin state
+    uint8_t count; // how many ticks it has been in this state
+};
+
+typedef enum {
+    DirectionCCW,
+    DirectionCW
+} Direction;
+
+#define CPU_PRESCALE(n)	(CLKPR = 0x80, CLKPR = (n))
+#define LED_CONFIG	(DDRD |= (1<<6))
+#define LED_OFF		(PORTD &= ~(1<<6))
+#define LED_ON		(PORTD |= (1<<6))
+
+static uint8_t const SwitchKeyMap[7] = {
+    KEY_2, // PORTB0 = S2
+    0,     // PORTB1 = A (dial)
+    KEY_1, // PORTB2 = S1
+    KEY_5, // PORTB3 = S5
+    KEY_4, // PORTB4 = S4
+    0,     // PORTB5 = B (dial)
+    KEY_3, // PORTB6 = S3
+};
+
+static uint8_t const DialA = 1; // PORTB2
+static uint8_t const DialB = 5; // PORTB5
+
+
 // Timer 0 clock select (prescaling; controls TCCR0B[2:0] aka CS0[2:0]).
 //
 // This controls how fast the system ticks.  We need to debounce
@@ -27,28 +60,9 @@ static uint8_t const DebounceTickLimit = 3;
 static volatile uint8_t _timer0_fired;
     // Default state: all high = nothing pressed
 static volatile uint8_t _raw_switches_state;
-static uint8_t debounced_switches_state = 0x7f;
 
-#define CPU_PRESCALE(n)	(CLKPR = 0x80, CLKPR = (n))
-#define LED_CONFIG	(DDRD |= (1<<6))
-#define LED_OFF		(PORTD &= ~(1<<6))
-#define LED_ON		(PORTD |= (1<<6))
-
-struct debounce_state {
-    uint8_t state; // pin state
-    uint8_t count; // how many ticks it has been in this state
-};
 static struct debounce_state switch_debounce_states[7];
-
-static uint8_t const switch_key_map[7] = {
-    KEY_B, // PORTB0 = S2
-    0,     // PORTB1 = A
-    KEY_C, // PORTB2 = S1
-    KEY_D, // PORTB3 = S5
-    KEY_E, // PORTB4 = S4
-    0,     // PORTB5 = B
-    KEY_F, // PORTB6 = S3
-};
+static uint8_t debounced_switches = 0x7f;
 
 static void setup(void) {
 	// 16 MHz clock speed
@@ -106,18 +120,22 @@ static uint8_t update_debounced_state(uint8_t raw_switches_state) {
                 
                 // Clear the bit for this switch, and then set it to
                 // the new, debounced value.
-                debounced_switches_state &= ~(0x01 << i);
-                debounced_switches_state |= (key_val << i);
+                debounced_switches &= ~(0x01 << i);
+                debounced_switches |= (key_val << i);
             }
             
         }
     }
     
-    return debounced_switches_state;
+    return debounced_switches;
 }
 
 static void run(void) {
     uint8_t last_pressed_keys = 0x7f;
+
+    uint8_t dial_moving = 0;
+    uint8_t dial_position = (PINB >> DialA) & 0x01;
+    Direction dial_direction = DirectionCCW;
     
 	for(;;) {        
         uint8_t timer0_fired = 0;
@@ -147,19 +165,46 @@ static void run(void) {
         if (timer0_fired) {            
             uint8_t pressed_keys = update_debounced_state(raw_switches_state);
             uint8_t changed_keys = last_pressed_keys ^ pressed_keys;
-            
+
+            // Process normal keys
             for(int i = 0; i < 7; i++) {
                 // A switch is pressed if it's logic low.
                 if ((((pressed_keys >> i) & 0x01) == 0) &&
                     ((changed_keys >> i) & 0x01)) {
 
-                    uint8_t key = switch_key_map[i];
+                    uint8_t key = SwitchKeyMap[i];
                     if (key != 0) {
                         usb_keyboard_press(key, 0);
                     }
                 }
             }
 
+            // Process dial
+            if (((pressed_keys >> DialA) & 0x01) != ((pressed_keys >> DialB) & 0x01)) {
+                // The dial inputs are different from one another, so
+                // it's moving now.
+                dial_moving = 1;
+                dial_direction = (((pressed_keys >> DialA) & 0x01) != dial_position) ? DirectionCW : DirectionCCW;
+            } else if (dial_moving) {
+                // Dial was moving but now has stopped, as indicated
+                // by the fact that the two inputs now the same
+                // value..
+                dial_moving = 0;
+                if (((pressed_keys >> DialA) & 0x01) != dial_position) {
+                    // Dial moved to new position.
+                    dial_position = ((pressed_keys >> DialA) & 0x01);
+                    if (dial_direction == DirectionCW) {
+                        usb_keyboard_press(KEY_VOLUME_UP, 0);
+                    } else {
+                        usb_keyboard_press(KEY_VOLUME_DOWN, 0);
+                    }
+                } else {
+                    // Dial returned to old position.  (Nothing to
+                    // do.)
+                }
+            }
+
+                
             last_pressed_keys = pressed_keys;
         }
 
@@ -177,10 +222,4 @@ int main(void) {
 ISR(TIMER0_OVF_vect) {
     _timer0_fired = 1;
     _raw_switches_state = PINB & 0x7f;
-        if (_raw_switches_state & 0x01) {
-            LED_OFF;
-        } else {
-            LED_ON;
-        }
-        
 }
